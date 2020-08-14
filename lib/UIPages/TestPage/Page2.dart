68091c6df4505dc1c8da3d3ba23c1b957d9938f7
fangbao <fangbao@hzxituan.com>
2020-08-14 10:05:55 +0800

    feat: context

---
+++ lib/UIPages/TestPage/Page2.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:xtflutter/UIPages/NormalUI/XTAppBackBar.dart';
+import 'package:xtflutter/XTConfig/AppConfig/XTRouter.dart';
+import 'package:xtflutter/Utils/Toast.dart';
+
+class TestPage2 extends StatefulWidget {
+  @override
+  _PageState createState() => _PageState();
+}
+
+class _PageState extends State<TestPage2> {
+  void _xtback(BuildContext context) {
+    XTRouter.closePage(context: context);
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: xtBackBar(title: "page2", back: () => _xtback(context)),
+      body: RaisedButton(
+        onPressed: () {
+          Toast.showToast(msg: 'page2');
+        },
+        child: Text('page2')
+      )
+    );
+  }
+}
