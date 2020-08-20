1eca178966e7f4730b98da19e7386e599fd2b2e0
fangbao <fangbao@hzxituan.com>
2020-08-20 21:56:06 +0800

    feat: XtNetError

--- lib/Utils/Error/Monitor.dart
+++ lib/Utils/Error/Monitor.dart
@@ -3,28 +3,27 @@ import 'package:flutter/material.dart';
 import 'package:xtflutter/Utils/Error/ReportError.dart';
 
 // 错误监控
-void monitor (cb) {
+void monitor(cb) {
   FlutterError.onError = (FlutterErrorDetails details) {
-    // print('111111111111-----------');
+    print('============= flutter error start =============');
     // print(details);
-    // print('--------111111111111');
+    print(details);
+    print('============= flutter error end =============');
     reportError(details);
   };
-  ErrorWidget.builder = (FlutterErrorDetails details){
+  ErrorWidget.builder = (FlutterErrorDetails details) {
     // print(details.toString());
     return Material(
-      child: Center(
+        child: Center(
       child: Text("Flutter 走神了"),
     ));
   };
   runZoned(() {
     cb();
   }, onError: (e, stack) {
-    // reportError(e.toString(), stack);
-    // print('error: start --------------------------------');
-    // print(e);
-    // print('---------------------------------------');
-    // print(stack);
-    // print('error: end --------------------------------');
+    print('============= zoned start =============');
+    print(e);
+    print(stack);
+    print('============= zoned end =============');
   });
 }
--- lib/Utils/Global.dart
+++ lib/Utils/Global.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
 class Global {
   static BuildContext _context;
   static BuildContext get context => _context;
-  static set context (dynamic c) {
+  static set context(dynamic c) {
     _context = c;
   }
+
+  static get isDebugger => kDebugMode;
 }
