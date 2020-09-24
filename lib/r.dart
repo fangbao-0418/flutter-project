848a29a2d65e17db057e31847128bbff18c912a1
pengxueliang <18365295532@163.com>
2020-09-24 11:24:08 +0800

    图片同步修改

--- lib/r.dart
+++ lib/r.dart
@@ -1,82 +1,84 @@
 class R {
-  /// ![](http://127.0.0.1:2222/images/Live/live_station_arrow_down.png)
-  static final String imagesLiveLiveStationArrowDown = 'images/Live/live_station_arrow_down.png';
-  /// ![](http://127.0.0.1:2222/images/Live/live_notify_horn.png)
-  static final String imagesLiveLiveNotifyHorn = 'images/Live/live_notify_horn.png';
-  /// ![](http://127.0.0.1:2222/images/Live/live_anchor_favorite_gray.png)
+  /// ![](http://127.0.0.1:9527/images/Live/live_anchor_favorite_gray.png)
   static final String imagesLiveLiveAnchorFavoriteGray = 'images/Live/live_anchor_favorite_gray.png';
-  /// ![](http://127.0.0.1:2222/images/Live/live_anchor_level_0.png)
-  static final String imagesLiveLiveAnchorLevel0 = 'images/Live/live_anchor_level_0.png';
-  /// ![](http://127.0.0.1:2222/images/Live/live_station_maicai_logo_selected.png)
-  static final String imagesLiveLiveStationMaicaiLogoSelected = 'images/Live/live_station_maicai_logo_selected.png';
-  /// ![](http://127.0.0.1:2222/images/Live/live_anchor_top_back.png)
-  static final String imagesLiveLiveAnchorTopBack = 'images/Live/live_anchor_top_back.png';
-  /// ![](http://127.0.0.1:2222/images/Live/live_anchor_top_bg.png)
-  static final String imagesLiveLiveAnchorTopBg = 'images/Live/live_anchor_top_bg.png';
-  /// ![](http://127.0.0.1:2222/images/Live/live_anchor_top_cancel_attention.png)
-  static final String imagesLiveLiveAnchorTopCancelAttention = 'images/Live/live_anchor_top_cancel_attention.png';
-  /// ![](http://127.0.0.1:2222/images/Live/live_anchor_top_white_share.png)
-  static final String imagesLiveLiveAnchorTopWhiteShare = 'images/Live/live_anchor_top_white_share.png';
-  /// ![](http://127.0.0.1:2222/images/Live/live_station_xituan_logo_selected.png)
-  static final String imagesLiveLiveStationXituanLogoSelected = 'images/Live/live_station_xituan_logo_selected.png';
-  /// ![](http://127.0.0.1:2222/images/Live/live_anchor_top_share.png)
-  static final String imagesLiveLiveAnchorTopShare = 'images/Live/live_anchor_top_share.png';
-  /// ![](http://127.0.0.1:2222/images/Live/live_station_top_bg.png)
-  static final String imagesLiveLiveStationTopBg = 'images/Live/live_station_top_bg.png';
-  /// ![](http://127.0.0.1:2222/images/Live/live_anchor_level_10.png)
-  static final String imagesLiveLiveAnchorLevel10 = 'images/Live/live_anchor_level_10.png';
-  /// ![](http://127.0.0.1:2222/images/Live/live_station_maicai_logo_normal.png)
-  static final String imagesLiveLiveStationMaicaiLogoNormal = 'images/Live/live_station_maicai_logo_normal.png';
-  /// ![](http://127.0.0.1:2222/images/Live/live_icon_play.png)
-  static final String imagesLiveLiveIconPlay = 'images/Live/live_icon_play.png';
-  /// ![](http://127.0.0.1:2222/images/Live/live_anchor_favorite_red.png)
+  /// ![](http://127.0.0.1:9527/images/Live/live_anchor_favorite_red.png)
   static final String imagesLiveLiveAnchorFavoriteRed = 'images/Live/live_anchor_favorite_red.png';
-  /// ![](http://127.0.0.1:2222/images/Live/live_station_arrow_right.png)
+  /// ![](http://127.0.0.1:9527/images/Live/live_anchor_level_0.png)
+  static final String imagesLiveLiveAnchorLevel0 = 'images/Live/live_anchor_level_0.png';
+  /// ![](http://127.0.0.1:9527/images/Live/live_anchor_level_10.png)
+  static final String imagesLiveLiveAnchorLevel10 = 'images/Live/live_anchor_level_10.png';
+  /// ![](http://127.0.0.1:9527/images/Live/live_anchor_top_back.png)
+  static final String imagesLiveLiveAnchorTopBack = 'images/Live/live_anchor_top_back.png';
+  /// ![](http://127.0.0.1:9527/images/Live/live_anchor_top_bg.png)
+  static final String imagesLiveLiveAnchorTopBg = 'images/Live/live_anchor_top_bg.png';
+  /// ![](http://127.0.0.1:9527/images/Live/live_anchor_top_cancel_attention.png)
+  static final String imagesLiveLiveAnchorTopCancelAttention = 'images/Live/live_anchor_top_cancel_attention.png';
+  /// ![](http://127.0.0.1:9527/images/Live/live_anchor_top_share.png)
+  static final String imagesLiveLiveAnchorTopShare = 'images/Live/live_anchor_top_share.png';
+  /// ![](http://127.0.0.1:9527/images/Live/live_anchor_top_white_share.png)
+  static final String imagesLiveLiveAnchorTopWhiteShare = 'images/Live/live_anchor_top_white_share.png';
+  /// ![](http://127.0.0.1:9527/images/Live/live_icon_play.png)
+  static final String imagesLiveLiveIconPlay = 'images/Live/live_icon_play.png';
+  /// ![](http://127.0.0.1:9527/images/Live/live_notify_horn.png)
+  static final String imagesLiveLiveNotifyHorn = 'images/Live/live_notify_horn.png';
+  /// ![](http://127.0.0.1:9527/images/Live/live_station_arrow_down.png)
+  static final String imagesLiveLiveStationArrowDown = 'images/Live/live_station_arrow_down.png';
+  /// ![](http://127.0.0.1:9527/images/Live/live_station_arrow_right.png)
   static final String imagesLiveLiveStationArrowRight = 'images/Live/live_station_arrow_right.png';
-  /// ![](http://127.0.0.1:2222/images/Live/live_station_xituan_logo_normal.png)
+  /// ![](http://127.0.0.1:9527/images/Live/live_station_maicai_logo_normal.png)
+  static final String imagesLiveLiveStationMaicaiLogoNormal = 'images/Live/live_station_maicai_logo_normal.png';
+  /// ![](http://127.0.0.1:9527/images/Live/live_station_maicai_logo_selected.png)
+  static final String imagesLiveLiveStationMaicaiLogoSelected = 'images/Live/live_station_maicai_logo_selected.png';
+  /// ![](http://127.0.0.1:9527/images/Live/live_station_top_bg.png)
+  static final String imagesLiveLiveStationTopBg = 'images/Live/live_station_top_bg.png';
+  /// ![](http://127.0.0.1:9527/images/Live/live_station_xituan_logo_normal.png)
   static final String imagesLiveLiveStationXituanLogoNormal = 'images/Live/live_station_xituan_logo_normal.png';
-  /// ![](http://127.0.0.1:2222/images/product_tag_global_s.png)
-  static final String imagesProductTagGlobalS = 'images/product_tag_global_s.png';
-  /// ![](http://127.0.0.1:2222/images/limit_time_seckill_shareBg_none.png)
-  static final String imagesLimitTimeSeckillShareBgNone = 'images/limit_time_seckill_shareBg_none.png';
-  /// ![](http://127.0.0.1:2222/images/share_wechat.png)
-  static final String imagesShareWechat = 'images/share_wechat.png';
-  /// ![](http://127.0.0.1:2222/images/wx_info_upload.jpg)
-  static final String imagesWxInfoUpload = 'images/wx_info_upload.jpg';
-  /// ![](http://127.0.0.1:2222/images/empty.png)
-  static final String imagesEmpty = 'images/empty.png';
-  /// ![](http://127.0.0.1:2222/images/my_address_list_edit.png)
-  static final String imagesMyAddressListEdit = 'images/my_address_list_edit.png';
-  /// ![](http://127.0.0.1:2222/images/header_realname.png)
-  static final String imagesHeaderRealname = 'images/header_realname.png';
-  /// ![](http://127.0.0.1:2222/images/pintuan_two.png)
-  static final String imagesPintuanTwo = 'images/pintuan_two.png';
-  /// ![](http://127.0.0.1:2222/images/edit_phone_icon.png)
+  /// ![](http://127.0.0.1:9527/images/Live/live_station_xituan_logo_selected.png)
+  static final String imagesLiveLiveStationXituanLogoSelected = 'images/Live/live_station_xituan_logo_selected.png';
+  /// ![](http://127.0.0.1:9527/images/.DS_Store)
+  static final String images = 'images/.DS_Store';
+  /// ![](http://127.0.0.1:9527/images/edit_phone_icon.png)
   static final String imagesEditPhoneIcon = 'images/edit_phone_icon.png';
-  /// ![](http://127.0.0.1:2222/images/product_sellOut_small.png)
-  static final String imagesProductSellOutSmall = 'images/product_sellOut_small.png';
-  /// ![](http://127.0.0.1:2222/images/tuanzi_refresh_header.gif)
-  static final String imagesTuanziRefreshHeader = 'images/tuanzi_refresh_header.gif';
-  /// ![](http://127.0.0.1:2222/images/share_save_img.png)
-  static final String imagesShareSaveImg = 'images/share_save_img.png';
-  /// ![](http://127.0.0.1:2222/images/limit_time_seckill_shareBg_two.png)
-  static final String imagesLimitTimeSeckillShareBgTwo = 'images/limit_time_seckill_shareBg_two.png';
-  /// ![](http://127.0.0.1:2222/images/joy.gif)
-  static final String imagesJoy = 'images/joy.gif';
-  /// ![](http://127.0.0.1:2222/images/product_tag_abroad_s.png)
-  static final String imagesProductTagAbroadS = 'images/product_tag_abroad_s.png';
-  /// ![](http://127.0.0.1:2222/images/img_message_empty.png)
-  static final String imagesImgMessageEmpty = 'images/img_message_empty.png';
-  /// ![](http://127.0.0.1:2222/images/limit_time_seckill_share.png)
-  static final String imagesLimitTimeSeckillShare = 'images/limit_time_seckill_share.png';
-  /// ![](http://127.0.0.1:2222/images/loading_round.gif)
-  static final String imagesLoadingRound = 'images/loading_round.gif';
-  /// ![](http://127.0.0.1:2222/images/xituan_logo.png)
-  static final String imagesXituanLogo = 'images/xituan_logo.png';
-  /// ![](http://127.0.0.1:2222/images/loading.png)
-  static final String imagesLoading = 'images/loading.png';
-  /// ![](http://127.0.0.1:2222/images/empty_name.png)
+  /// ![](http://127.0.0.1:9527/images/empty.png)
+  static final String imagesEmpty = 'images/empty.png';
+  /// ![](http://127.0.0.1:9527/images/empty_name.png)
   static final String imagesEmptyName = 'images/empty_name.png';
-  /// ![](http://127.0.0.1:2222/images/limit_time_seckill_shareBg.png)
+  /// ![](http://127.0.0.1:9527/images/header_realname.png)
+  static final String imagesHeaderRealname = 'images/header_realname.png';
+  /// ![](http://127.0.0.1:9527/images/img_message_empty.png)
+  static final String imagesImgMessageEmpty = 'images/img_message_empty.png';
+  /// ![](http://127.0.0.1:9527/images/joy.gif)
+  static final String imagesJoy = 'images/joy.gif';
+  /// ![](http://127.0.0.1:9527/images/limit_time_seckill_share.png)
+  static final String imagesLimitTimeSeckillShare = 'images/limit_time_seckill_share.png';
+  /// ![](http://127.0.0.1:9527/images/limit_time_seckill_shareBg.png)
   static final String imagesLimitTimeSeckillShareBg = 'images/limit_time_seckill_shareBg.png';
+  /// ![](http://127.0.0.1:9527/images/limit_time_seckill_shareBg_none.png)
+  static final String imagesLimitTimeSeckillShareBgNone = 'images/limit_time_seckill_shareBg_none.png';
+  /// ![](http://127.0.0.1:9527/images/limit_time_seckill_shareBg_two.png)
+  static final String imagesLimitTimeSeckillShareBgTwo = 'images/limit_time_seckill_shareBg_two.png';
+  /// ![](http://127.0.0.1:9527/images/loading.png)
+  static final String imagesLoading = 'images/loading.png';
+  /// ![](http://127.0.0.1:9527/images/loading_round.gif)
+  static final String imagesLoadingRound = 'images/loading_round.gif';
+  /// ![](http://127.0.0.1:9527/images/my_address_list_edit.png)
+  static final String imagesMyAddressListEdit = 'images/my_address_list_edit.png';
+  /// ![](http://127.0.0.1:9527/images/pintuan_two.png)
+  static final String imagesPintuanTwo = 'images/pintuan_two.png';
+  /// ![](http://127.0.0.1:9527/images/product_sellOut_small.png)
+  static final String imagesProductSellOutSmall = 'images/product_sellOut_small.png';
+  /// ![](http://127.0.0.1:9527/images/product_tag_abroad_s.png)
+  static final String imagesProductTagAbroadS = 'images/product_tag_abroad_s.png';
+  /// ![](http://127.0.0.1:9527/images/product_tag_global_s.png)
+  static final String imagesProductTagGlobalS = 'images/product_tag_global_s.png';
+  /// ![](http://127.0.0.1:9527/images/share_save_img.png)
+  static final String imagesShareSaveImg = 'images/share_save_img.png';
+  /// ![](http://127.0.0.1:9527/images/share_wechat.png)
+  static final String imagesShareWechat = 'images/share_wechat.png';
+  /// ![](http://127.0.0.1:9527/images/tuanzi_refresh_header.gif)
+  static final String imagesTuanziRefreshHeader = 'images/tuanzi_refresh_header.gif';
+  /// ![](http://127.0.0.1:9527/images/wx_info_upload.jpg)
+  static final String imagesWxInfoUpload = 'images/wx_info_upload.jpg';
+  /// ![](http://127.0.0.1:9527/images/xituan_logo.png)
+  static final String imagesXituanLogo = 'images/xituan_logo.png';
 }
